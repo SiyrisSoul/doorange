76b6f8c81363c2e52a9594ff5ef41840df6ecfd9
Siyris <hello@siyris.com>
2024-07-22 15:19:55 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'cowpea' {
+declare module 'doorange' {
   export function rhyme(input: string): Promise<string[]>;
   export function synonym(input: string): Promise<string[]>;
   export function find(word1: string, word2: string): Promise<{ word: string[]; swap: string[]; description: string; }[]>;
